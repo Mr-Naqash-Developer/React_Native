622440151f8d1c5a4d82b61f97e1be5b2dfde502
Mr.Naqash.Developer <mr.naqash.developer@gmail.com>
2025-03-10 14:10:04 +0500

    I've completed the chapter:4

--- app/index.jsx
+++ app/index.jsx
@@ -2,17 +2,23 @@
 import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-import coffeeLogo from '@/assets/images/coffee-logo.jpg'
+import mainImg from '@/assets/images/iced-coffee.png'
 
 const app = () => {
   return (
     <View style={style.container}>
       <ImageBackground
-        source={coffeeLogo}
+        source={mainImg}
         resizeMode='cover'
         style={style.image}
       >
         <Text style={style.title}>Coffee Shop</Text>
+
+        <Link style={{ marginHorizontal: 'auto' }} href="/menu" asChild>
+          <Pressable style={style.button}>
+            <Text style={style.buttonText}>Our Menu</Text>
+          </Pressable>
+        </Link>
 
         <Link style={{ marginHorizontal: 'auto' }} href="/contact" asChild>
           <Pressable style={style.button}>
@@ -58,10 +64,12 @@ const style = StyleSheet.create({
   },
   button: {
     height: 60,
+    width: 150,
     borderRadius: 20,
     backgroundColor: 'rgba(0,0,0,0.75)',
     padding: 6,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginBottom: 50,
   },
   buttonText: {
     color: 'white',
